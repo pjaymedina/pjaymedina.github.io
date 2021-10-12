db1aee5c1b8a6e6748ecf8884a899b349c0e5d18
Pjay Medina <pjay.medina@gmail.com>
2021-10-13 00:00:25 +0800

    portfolio update

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as HomePage } from "./home";
+export { default as AboutPage } from "./about";
+export { default as SkillsPage } from "./skills";
+export { default as ContactPage } from "./contact";
+export { default as PortfolioPage } from "./portfolio";
